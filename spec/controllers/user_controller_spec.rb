c5fdbd485d8a2441d685aa997ceb7413fe5fac55
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-02-23 15:08:17 +0000

    Updated user

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe UserController, type: :controller do
 
-  describe "GET #index" do
+  describe "GET /user/index" do
     it "returns http success" do
-      get :index
+      get :new
       expect(response).to have_http_status(:success)
     end
   end
