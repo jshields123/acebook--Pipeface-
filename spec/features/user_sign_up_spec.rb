50c3d69aeef63e3f07757e30c4bc5e248422534b
megan gosnell <megan_gosnell@aol.co.uk>
2021-02-23 12:33:59 +0000

    user sign in files created

---
+++ spec/features/user_sign_up_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+feature "visitor signs up" do
+    scenario "a user can sign up with an email and password" do
+     visit('/user/index')
+     fill_in("name", with: "Test name")
+     fill_in("email", with: "hello@email.com")
+     fill_in("password", with: "password")
+     click_button "Submit"
+     expect(page).to have_content("Signed up")
+    end 
+end 
