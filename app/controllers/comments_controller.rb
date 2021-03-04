0a299535fb5fb49613c37d88d3b8a110938ba8d6
megan gosnell <megan_gosnell@aol.co.uk>
2021-03-04 11:04:08 +0000

    a user can delete their comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,27 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  # before_action :correct_user, only: %i[edit update destroy]
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     redirect_to '/posts'
   end
 
+  def destroy 
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to '/posts'
+  end 
+  
   private
 
   def comment_params
     params.require(:comment).permit(:body)
   end
-end
+
+  # def correct_user 
+  #   @comment = Comment.find(params[:id])
+  #   redirect_to comments_path, notice: 'No access' unless @comment.user == current_user
+  # end
+end 
